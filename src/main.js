88892b28f092555594c52c608e231ea103f1840a
v-yuanye <v-yuanyeu@guazi.com>
2020-07-08 16:32:10 +0800

    feat✨ 重置样式，全局样式，300ms点击

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+// 重置css样式
+import 'normalize.css/normalize.css'
+// global css 全局公用js
+import '@/styles/index.scss'
 // 移动端适配方案
 import 'amfe-flexible/index.js'
+// 解决click事件300ms延迟
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
 
 import router from './router'
 import store from './store'
